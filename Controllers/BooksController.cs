e31ce2751b55aa66e8103e7fb0e7b8925dcd070d
JekenMaharjan <maharjanjeken@gmail.com>
2026-01-21 16:20:40 +0545

    Implement GET and POST actions for BooksController

--- Controllers/BooksController.cs
+++ Controllers/BooksController.cs
@@ -1,6 +1,32 @@
-﻿namespace Library_Management_System.Controllers
+﻿using Library_Management_System.Data;
+using Library_Management_System.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Library_Management_System.Controllers
 {
-    public class BooksController
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BooksController : ControllerBase
     {
+        private readonly AppDbContext _context;
+
+        public BooksController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetBooks()
+        {
+            return Ok(_context.Books.ToList());
+        }
+
+        [HttpPost]
+        public IActionResult AddBook(Book book)
+        {
+            _context.Books.Add(book);
+            _context.SaveChanges();
+            return Ok(book);
+        }
     }
 }
